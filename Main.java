9a270426a5570b5d9f428594fb5b6c84c96cd928
Reem Belafkih <130573894+reembe@users.noreply.github.com>
2024-04-27 20:30:16 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class TNTRun {
+public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int T = scanner.nextInt(); // Number of test cases
